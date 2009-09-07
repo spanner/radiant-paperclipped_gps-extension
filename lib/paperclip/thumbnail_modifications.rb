66966a4855795c575f4b849910546d6d9f769c80
will <will@spanner.org>
2009-09-07 14:40:16 +0100

    stack fixed properly this time

--- lib/paperclip/thumbnail_modifications.rb
+++ lib/paperclip/thumbnail_modifications.rb
@@ -2,15 +2,15 @@ module Paperclip
   module ThumbnailModifications
 
     def self.included(base)
-      base.class_eval {
-
-        def initialize_with_gps file, options = {}, attachment = nil
-          raise PaperclipError, "Skipping non-image thumbnail rule #{options[:format]}" if options[:geometry].nil?
-          initialize_without_gps(file, options, attachment)
-        end
-        alias_method_chain :initialize, :gps
-
-      }
+      unless base.methods.include?('initialize_without_gps')
+        base.class_eval {
+          def initialize_with_gps file, options = {}, attachment = nil
+            raise PaperclipError, "Skipping non-image thumbnail rule #{options[:format]}" if options[:geometry].nil?
+            initialize_without_gps(file, options, attachment)
+          end
+          alias_method_chain :initialize, :gps
+        }
+      end
     end
   end
 end
