b602c70631a485014bc25e339e08f63c58d64f55
will <will@spanner.org>
2009-11-11 12:48:00 +0000

    javascript updated

--- public/javascripts/admin/map_callbacks.js
+++ public/javascripts/admin/map_callbacks.js
@@ -20,12 +20,26 @@ function MM_showRoute(mapviewer, request) {
       var xmlText = request.responseText;
       var xmlDoc = mapviewer.parseXML( xmlText );
       var markers = xmlDoc.documentElement.getElementsByTagName( 'trkpt' );
+      if (markers.length == 0) markers = xmlDoc.documentElement.getElementsByTagName( 'rtept' );
       var points = [];
       for( var i = 0; i < markers.length && i < 1000; ++i ) {
         var point = new MMLatLon(
-        parseFloat(markers[i].getAttribute('lat')),
-        parseFloat(markers[i].getAttribute('lon')) );
+          parseFloat(markers[i].getAttribute('lat')),
+          parseFloat(markers[i].getAttribute('lon')) 
+        );
         points.push( point );
+        var label = null;
+        if (i == 0) {
+          mapviewer.createMarker( point, { zIndex: 1001, 'text' : '>', 'label' : 'start'} );
+        } else if (i == markers.length) {
+          mapviewer.createMarker( point, { zIndex: 1002, 'text' : '@', 'label' : 'finish'} );
+        } else {
+          var name_element = markers[i].getElementsByTagName('name')[0];
+          label = name_element ? name_element.firstChild.nodeValue : markers[i].getAttribute('title');
+          if (label && !label.match(/^WP/)) {
+            mapviewer.createMarker( point, { zIndex: 1003, 'text' : '!', 'label' : label} ).setInfoBoxContent("<p>" + label + "</p>");
+          }
+        }
       }
       polyline = new MMPolyLineOverlay( points, undefined, 0.4, 1, false, undefined );
       var location = mapviewer.getAutoScaleLocation( points );
